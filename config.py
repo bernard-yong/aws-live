03bc1484080f0adb064c790cc09fa1d9eb35682d
bernard-yong <130631222+bernard-yong@users.noreply.github.com>
2023-04-13 12:44:38 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cryqgq9lp8wy.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yongxianye-employee"
+customregion = "us-east-1"
